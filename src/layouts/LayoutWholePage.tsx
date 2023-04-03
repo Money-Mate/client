e7b386b93666df2531e3d7a9c23d63fe6377420d
QD2022 <104011263+QD2022@users.noreply.github.com>
2023-04-03 15:18:42 +0200

    Placeholder Navbar

--- src/layouts/LayoutWholePage.tsx
+++ src/layouts/LayoutWholePage.tsx
@@ -1,10 +1,12 @@
 
 import LayoutWholeMain from './LayoutWholeMain'
+import Nav from './Nav'
 
 function Layout() {
   return (
     <div className="grid grid-cols-12 h-screen">
-        <div className="col-span-3 bg-indigo-800 h-full ">sidebar-placeholder</div>
+        <div className="col-span-3 bg-indigo-800 h-full">
+        <Nav/> </div>
         <div className="col-span-9 h-full"><LayoutWholeMain/></div>
     </div>
   )
