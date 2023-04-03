043d928061a765bec652563a2bc739d20354600c
Pterodactylae <leonie.schwitzkowski@dci-student.de>
2023-04-03 15:41:59 +0200

    repaired signIn

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, FormEvent } from "react";
 import "../index.css"; // import the Tailwind CSS file
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-
 
 const SignIn = () => {
   const navigate = useNavigate();
@@ -10,9 +9,8 @@ const SignIn = () => {
     email: "",
     password: "",
   });
-  // const { setUser, setIsLoggedIn } = useStore();
 
-  const handleSubmit = async (e:any) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     const BE_URL = import.meta.env.VITE_BE_PORT;
     e.preventDefault();
     try {
@@ -23,19 +21,17 @@ const SignIn = () => {
           withCredentials: true,
         }
       );
-      console.log(res.data);
-    //   localStorage.setItem("user", JSON.stringify(res.data.user));
-    //   // setIsLoggedIn(true)
-    //   // setUser(res.data.user);
-    //   console.log(res.data)
-    //   if (localStorage.getItem('user')) {
-    //     console.log("user in localstorage")
-    //     // navigate("/");
-    //   }
+      console.log(res.data)
+      localStorage.setItem("user", JSON.stringify(res.data.user));
+      if (localStorage.getItem('user')) {
+        console.log("user in localstorage")
+        navigate("/");
+      }
     } catch (error) {
       console.log(error);
     }
   };
+  
   return (
     <div className="flex h-screen bg-gray-200">
       {/* Left Side - Image */}
@@ -50,54 +46,58 @@ const SignIn = () => {
       <div className="flex items-center justify-center w-1/2 bg-white">
         <div className="w-full max-w-md">
           <h1 className="text-2xl font-bold mb-6">Login</h1>
+          <form onSubmit={handleSubmit}>
 
-          {/* Email Input */}
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 font-bold mb-2"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              className="border rounded-md py-2 px-3 text-gray-700 w-full"
-              id="email"
-              type="email"
-              placeholder="Enter your email"
-              onChange={(e) =>
-                setUserLogin({ ...userLogin, email: e.target.value })
-              }
-            />
-          </div>
+            {/* Email Input */}
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 font-bold mb-2"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                className="border rounded-md py-2 px-3 text-gray-700 w-full"
+                id="email"
+                type="email"
+                placeholder="Enter your email"
+                value={userLogin.email}
+                onChange={(e) =>
+                  setUserLogin({ ...userLogin, email: e.target.value })
+                }
+              />
+            </div>
 
-          {/* Password Input */}
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 font-bold mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              className="border rounded-md py-2 px-3 text-gray-700 w-full"
-              id="password"
-              type="password"
-              placeholder="Enter your password"
-              onChange={(e) =>
-                setUserLogin({ ...userLogin, password: e.target.value })
-              }
-            />
-          </div>
+            {/* Password Input */}
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="border rounded-md py-2 px-3 text-gray-700 w-full"
+                id="password"
+                type="password"
+                placeholder="Enter your password"
+                value={userLogin.password}
+                onChange={(e) =>
+                  setUserLogin({ ...userLogin, password: e.target.value })
+                }
+              />
+            </div>
 
-          {/* Login Button */}
-          <div className="mb-6">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              onClick={handleSubmit}
-            >
-              Login
-            </button>
-          </div>
+            {/* Login Button */}
+            <div className="mb-6">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                type="submit"
+              >
+                Login
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
