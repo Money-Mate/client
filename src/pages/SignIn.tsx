58f1dc2465182d8bc87efa614dacd21e61059768
Pterodactylae <leonie.schwitzkowski@dci-student.de>
2023-04-03 14:29:41 +0200

    added routes, signIn and SignUp Page

---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import "../index.css"; // import the Tailwind CSS file
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+
+const SignIn = () => {
+  const navigate = useNavigate();
+  const [userLogin, setUserLogin] = useState({
+    email: "",
+    password: "",
+  });
+  // const { setUser, setIsLoggedIn } = useStore();
+
+  const handleSubmit = async (e:any) => {
+    // const BE_URL = import.meta.env.VITE_BE_PORT;
+    // e.preventDefault();
+    // try {
+    //   const res = await axios.post(
+    //     `${BE_URL}/user/login`,
+    //     userLogin,
+    //     {
+    //       withCredentials: true,
+    //     }
+    //   );
+    //   localStorage.setItem("user", JSON.stringify(res.data.user));
+    //   // setIsLoggedIn(true)
+    //   // setUser(res.data.user);
+    //   console.log(res.data)
+    //   if (localStorage.getItem('user')) {
+    //     console.log("user in localstorage")
+    //     // navigate("/");
+    //   }
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  };
+  return (
+    <div className="flex h-screen bg-gray-200">
+      {/* Left Side - Image */}
+      <div
+        className="w-1/2 bg-cover bg-center"
+        style={{
+          backgroundImage: "url('https://picsum.photos/id/237/1000/1000')",
+        }}
+      ></div>
+
+      {/* Right Side - Login Form */}
+      <div className="flex items-center justify-center w-1/2 bg-white">
+        <div className="w-full max-w-md">
+          <h1 className="text-2xl font-bold mb-6">Login</h1>
+
+          {/* Email Input */}
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="border rounded-md py-2 px-3 text-gray-700 w-full"
+              id="email"
+              type="email"
+              placeholder="Enter your email"
+              onChange={(e) =>
+                setUserLogin({ ...userLogin, email: e.target.value })
+              }
+            />
+          </div>
+
+          {/* Password Input */}
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="border rounded-md py-2 px-3 text-gray-700 w-full"
+              id="password"
+              type="password"
+              placeholder="Enter your password"
+              onChange={(e) =>
+                setUserLogin({ ...userLogin, password: e.target.value })
+              }
+            />
+          </div>
+
+          {/* Login Button */}
+          <div className="mb-6">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={handleSubmit}
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const SignUp = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const navigate= useNavigate();
+
+  const handleChange = (e:any) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e:any) => {
+    e.preventDefault();
+    const BE_URL = import.meta.env.VITE_BE_PORT;
+    try {
+      const res = await axios.post(`${BE_URL}/user/register`, formData);
+      console.log(res.data);
+      navigate("/login")
+    } catch (err:any) {
+      console.error(err.response.data);
+    }
+  };
+
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h2 className="text-2xl font-bold mb-4">Register</h2>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="city">
+            Username
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="city"
+            type="text"
+            name="city"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Register
+          </button>
+        </div>
+      </form>
+    </div>
+);
+};
+
+export default SignUp;    
