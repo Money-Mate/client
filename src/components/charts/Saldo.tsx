6ba4f31567628daf727f007f1dd2a9a4af60a4a8
Pterodactylae <leonie.schwitzkowski@dci-student.de>
2023-04-05 15:53:43 +0200

    clean up

--- src/components/charts/Saldo.tsx
+++ src/components/charts/Saldo.tsx
@@ -1,29 +1,28 @@
-import React from 'react';
-import { Bar } from 'react-chartjs-2';
-import { ChartData, ChartOptions } from 'chart.js/auto';
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import { ChartData, ChartOptions } from "chart.js/auto";
 
 const getBarColors = (values: number[]): string[] => {
-    return values.map((value, index) => {
-      const opacity = index === values.length - 1 ? '80' : 'ff';
-      return value >= 0 ? `#2ECC40${opacity}` : `#FF4136${opacity}`;
-    });
-  };
+  return values.map((value, index) => {
+    const opacity = index === values.length - 1 ? "60" : "ff";
+    return value >= 0 ? `#2ECC40${opacity}` : `#FF4136${opacity}`;
+  });
+};
 
 const Saldo = () => {
-
-  const data: ChartData<'bar'> = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-  datasets: [
-  {
-    label: 'Saldo',
-    data: [-70, -60, 3, 50, -100, 300],
-    backgroundColor: getBarColors([-70, -60, 3, 50, -100, 300])
-  }
-]
+  const data: ChartData<"bar"> = {
+    labels: ["January", "February", "March", "April", "May", "June"],
+    datasets: [
+      {
+        label: "Saldo",
+        data: [-70, -60, 3, 50, -100, 300],
+        backgroundColor: getBarColors([-70, -60, 3, 50, -100, 300]),
+      },
+    ],
   };
 
-  const options: ChartOptions<'bar'> = {
-    indexAxis: 'x',
+  const options: ChartOptions<"bar"> = {
+    indexAxis: "x",
     elements: {
       bar: {
         borderWidth: 2,
@@ -32,24 +31,23 @@ const Saldo = () => {
     responsive: true,
     plugins: {
       legend: {
-        position: 'top',
-      display: false,
-
+        position: "top",
+        display: false,
       },
       title: {
         display: false,
-        text: 'Saldo Chart',
+        text: "Saldo Chart",
       },
     },
     scales: {
       y: {
         ticks: {
-          callback: function(value) {
-            return '€' + value;
-          }
-        }
-      }
-    }
+          callback: function (value) {
+            return "€" + value;
+          },
+        },
+      },
+    },
   };
 
   return <Bar data={data} options={options} />;
