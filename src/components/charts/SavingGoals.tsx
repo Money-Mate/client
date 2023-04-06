81f4d12d24c5f2d9e7c6ad96b3fcea92db55482c
Pterodactylae <leonie.schwitzkowski@dci-student.de>
2023-04-06 11:49:58 +0200

    merged feature/charts/Notgroschen into main

--- src/components/charts/SavingGoals.tsx
+++ src/components/charts/SavingGoals.tsx
@@ -1,12 +1,15 @@
 import {
-  BarElement,
-  CategoryScale,
   Chart as ChartJS,
+  CategoryScale,
   LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
 } from "chart.js";
 import { Bar } from "react-chartjs-2";
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
-ChartJS.register(CategoryScale, LinearScale, BarElement);
 
 interface SavingsGoals {
   [key: string]: number;
