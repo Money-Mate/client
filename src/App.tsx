9d8720a37fd119e439f515001657143d09aaa4f6
Pterodactylae <leonie.schwitzkowski@dci-student.de>
2023-04-18 10:50:25 +0200

    cahnged some layoutrouting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import "./App.css";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Layout from "./layouts/LayoutPage";
--- src/layouts/LayoutContent.tsx
+++ src/layouts/LayoutContent.tsx
@@ -2,10 +2,10 @@ import React, { ReactElement } from "react";
 import Header from "./Header";
 import Main from "./Main";
 import { useRoutes } from "react-router-dom";
-import routes from "../routes/routes";
+import contentRoutes from "../routes/contentRoutes";
 
 function LayoutContent(): ReactElement {
-  const element = useRoutes(routes);
+  const element = useRoutes(contentRoutes);
 
   return (
     <div className="flex flex-col h-screen w-full bg-neutral-200">
