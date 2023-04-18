c96d539f76098a97e4770befec2d3e3b2db3ae6e
Pterodactylae <leonie.schwitzkowski@dci-student.de>
2023-04-18 16:18:52 +0200

    added transactionsGiroComponent and TransactionModal CRUD, not working

--- src/pages/TransactionsGiro.tsx
+++ src/pages/TransactionsGiro.tsx
@@ -1,80 +1,123 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Modal from "../components/Modals/EditTransactions";
 
-
-//fetch 
-interface TableRowData {
-  Konto: string;
-  Summe: number;
-  Währung: string;
-  Empfänger: string;
-  Verwendungszweck: string;
+interface TransactionData {
+  _id: string;
+  accountIBAN: string;
+  date: string;
+  transactionText: string;
+  recipientName: string;
+  recipientIBAN: string;
+  amount: number;
+  currency: string;
+  category: string;
+  subCategory: string;
+  tags: string[];
 }
-
-const tableData: TableRowData[] = [
-  {
-    Konto: "Konto 1",
-    Summe: -100.0,
-    Währung: "EUR",
-    Empfänger: "Max Mustermann",
-    Verwendungszweck: "Miete",
-  },
-  {
-    Konto: "Konto 2",
-    Summe: -200.0,
-    Währung: "USD",
-    Empfänger: "John Doe",
-    Verwendungszweck: "Shopping",
-  },
-  {
-    Konto: "Konto 3",
-    Summe: -50.0,
-    Währung: "EUR",
-    Empfänger: "Jane Doe",
-    Verwendungszweck: "Geschenk",
-  },
-];
+const BE_URL= import.meta.env.VITE_BE_PORT
 
 const Table = () => {
-  const handleEditRow = (index: number) => {
-    console.log(`Edit row ${index}`);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [editingRowIndex, setEditingRowIndex] = useState(-1);
+  const [tableDataState, setTableDataState] = useState<TransactionData[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${BE_URL}/transaction/getMy`, {
+          withCredentials: true,
+        });
+        console.log(response.data);
+        setTableDataState(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [isModalOpen]);
+
+
+  const handleRowAction = async (index: number, rowData?: TransactionData) => {
+    if (rowData) {
+      try {
+        const response = await axios.put(
+  `${BE_URL}/transaction/updateMy`,
+  { transactionId: rowData._id, data: rowData },
+  { withCredentials: true }
+);
+        
+        // Update the data in the table
+        const newData = [...tableDataState];
+        newData[index] = response.data;
+        setTableDataState(newData);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  
+    // Close the modal
+    setIsModalOpen(false);
+    setEditingRowIndex(-1);
   };
+  
+  // Get unique categories from the data
+  const categories = Array.from(new Set(tableDataState.map((row) => row.category)));
 
   return (
     <div className="overflow-x-auto">
-      <table className="table-auto w-full">
-        <thead>
-          <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-            <th className="py-3 px-6 text-left font-bold">Konto</th>
-            <th className="py-3 px-6 text-left font-bold">Summe</th>
-            <th className="py-3 px-6 text-left font-bold">Währung</th>
-            <th className="py-3 px-6 text-left font-bold">Empfänger</th>
-            <th className="py-3 px-6 text-left font-bold">Verwendungszweck</th>
-            <th className="py-3 px-6 text-left font-bold">Aktionen</th>
+    <table className="table-auto w-full">
+      <thead>
+        <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+          <th className="py-3 px-6 text-left font-bold">Konto</th>
+          <th className="py-3 px-6 text-left font-bold">Summe</th>
+          <th className="py-3 px-6 text-left font-bold">Währung</th>
+          <th className="py-3 px-6 text-left font-bold">Empfänger</th>
+          <th className="py-3 px-6 text-left font-bold">Verwendungszweck</th>
+          <th className="py-3 px-6 text-left font-bold">Datum</th>
+          <th className="py-3 px-6 text-left font-bold">Aktionen</th>
+        </tr>
+      </thead>
+      <tbody className="text-gray-600 text-sm font-light">
+        {tableDataState.map((row, index) => (
+          <tr key={row._id} className={index % 2 === 0 ? "bg-gray-100" : ""}>
+            <td className="py-3 px-6 text-left whitespace-nowrap">
+              {row.accountIBAN}
+            </td>
+            <td className="py-3 px-6 text-left whitespace-nowrap">
+              {row.amount ? row.amount.toFixed(2) : ""} {row.currency}
+            </td>
+            <td className="py-3 px-6 text-left whitespace-nowrap">
+              {row.currency}
+            </td>
+            <td className="py-3 px-6 text-left">{row.recipientName}</td>
+            <td className="py-3 px-6 text-left">{row.transactionText}</td>
+            <td className="py-3 px-6 text-left"> {row.date}</td>
+            <td>
+              <button
+                className="text-red-400"
+                onClick={() => {
+                  setEditingRowIndex(index);
+                  setIsModalOpen(true);
+                }}
+              >
+                Bearbeiten
+              </button>
+            </td>
           </tr>
-        </thead>
-        <tbody className="text-gray-600 text-sm font-light">
-          {tableData.map((row, index) => (
-            <tr key={index} className={index % 2 === 0 ? "bg-gray-100" : ""}>
-              <td className="py-3 px-6 text-left whitespace-nowrap">
-                {row.Konto}
-              </td>
-              <td className="py-3 px-6 text-left whitespace-nowrap">
-                {row.Summe.toFixed(2)} {row.Währung}
-              </td>
-              <td className="py-3 px-6 text-left whitespace-nowrap">
-                {row.Währung}
-              </td>
-              <td className="py-3 px-6 text-left">{row.Empfänger}</td>
-              <td className="py-3 px-6 text-left">{row.Verwendungszweck}</td>
-              <td className="py-3 px-6 text-left">
-                <button className="text-red-400" onClick={() => handleEditRow(index)}>Bearbeiten</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+        ))}
+      </tbody>
+    </table>
+    {isModalOpen && editingRowIndex >= 0 && (
+      <Modal
+        title="Bearbeiten"
+        onSave={(rowData) => handleRowAction(editingRowIndex, rowData)}
+        onCancel={() => handleRowAction(editingRowIndex)}
+        data={tableDataState[editingRowIndex]}
+        categories={categories}
+      />
+    )}
+  </div>
   );
-};
-
-export default Table;
+      };
+      export default Table;
