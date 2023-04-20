44255849cd7de23681a9935ce3d51a1b8b0cce0b
Pterodactylae <leonie.schwitzkowski@dci-student.de>
2023-04-20 17:48:48 +0200

    clean up

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,6 +1,6 @@
-import axios from 'axios';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
   const navigate = useNavigate();
@@ -8,27 +8,25 @@ function LandingPage() {
   useEffect(() => {
     async function checkLoggedIn() {
       try {
-        const BE_URL= import.meta.env.VITE_BE_PORT;
+        const BE_URL = import.meta.env.VITE_BE_PORT;
         const response = await axios.get(`${BE_URL}/user/checkToken`, {
           withCredentials: true,
         });
-        if (response.data === true ) {
-          navigate('/app/userdashboard');
+        if (response.data === true) {
+          navigate("/app/userdashboard");
         }
-        console.log(response.data)
       } catch (error) {
         console.log(error);
       }
     }
-
     checkLoggedIn();
   }, []);
 
   return (
     <div>
       <h1>Landing Page</h1>
-      <button onClick={() => navigate('/signin')}>Sign In</button>
-      <button onClick={() => navigate('/signup')}>Register</button>
+      <button onClick={() => navigate("/signin")}>Sign In</button>
+      <button onClick={() => navigate("/signup")}>Register</button>
     </div>
   );
 }
