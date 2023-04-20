d2d60dc1e7e2ed9b64ea2d1fc65366942a8f850f
Pterodactylae <leonie.schwitzkowski@dci-student.de>
2023-04-20 15:19:15 +0200

    added navigation to landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function LandingPage() {
+  const navigate= useNavigate()
   return (
-    <div>LandingPage</div>
+    <div>LandingPage
+      <button onClick={()=> navigate(`/signin`) } className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">SignIn</button>
+      <button onClick={()=> navigate(`/signup`) } className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Register</button>
+      <button onClick={()=> navigate(`/app/userdashboard`) } className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Dashboard</button>
+    </div>
   )
 }
 
