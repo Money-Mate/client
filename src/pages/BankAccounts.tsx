10f605c9965ed73f8298ec319134d9eb1b308d60
SebastianWolter1 <seb@techtakel.berlin>
2023-04-23 11:13:35 +0200

    update

--- src/pages/BankAccounts.tsx
+++ src/pages/BankAccounts.tsx
@@ -1,104 +1,163 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
-import useAccountStore from "../context/Accountstore";
-import { IBankAccountData } from "../context/Accountstore";
 
-const BankAccounts = () => {
-  const { bankAccountData, fetchBankAccountData, addBankAccount, updateBankAccount, deleteBankAccount } = useAccountStore();
-  const [showModal, setShowModal] = useState(false);
-  const [newAccount, setNewAccount] = useState<IBankAccountData>({ _id: "", name: "", iban: "", reference: "name"});
 
-  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNewAccount({ ...newAccount, name: event.target.value });
-  };
-
-  const handleIbanChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNewAccount({ ...newAccount, iban: event.target.value });
-  };
-
-  const handleReferenceChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setNewAccount({ ...newAccount, reference: event.target.value as "name" | "iban"});
-  };
-
-  const handleAddAccount = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    addBankAccount(newAccount);
-    setNewAccount({ _id: "", name: "", iban: "", reference: "name" });
-  };
-
-  const handleUpdateAccount = (event: React.MouseEvent<HTMLButtonElement>, account: IBankAccountData) => {
-    event.preventDefault();
-    console.log(account);
-    updateBankAccount(account);
-  };
-
-  const handleDeleteAccount = (event: React.MouseEvent<HTMLButtonElement>, id: string) => {
-    event.preventDefault();
-    deleteBankAccount(id);
-    setShowModal(false)
-  };
-
-  useEffect(() => {
-    fetchBankAccountData();
-  }, []);
-
-  return (
-    <div>
-      <h2>Bank Accounts</h2>
-      <ul>
-        {bankAccountData && bankAccountData.map((account) => (
-          <li key={account._id}>
-            <form>
-              <label>
-                Name:
-                <input type="text" value={account.name} onChange={(event) => updateBankAccount({ ...account, name: event.target.value })} />
-              </label>
-              <label>
-                IBAN:
-                <input type="text" value={account.iban} onChange={(event) => updateBankAccount({ ...account, iban: event.target.value })} />
-              </label>
-              <label>
-                Reference:
-                <select value={account.reference} onChange={(event) => updateBankAccount({ ...account, reference: event.target.value as "name" | "iban" })}>
-                  <option value="name">Name</option>
-                  <option value="iban">IBAN</option>
-                </select>
-              </label>
-              <button onClick={(event) => handleUpdateAccount(event, account)}>Update</button>
-              <button onClick={()=> setShowModal(true)}>Delete</button>
-            </form>
-              {showModal && (
-                <div>
-                  <h1>Are you sure you want to delete this account?</h1>
-                  <button onClick={() => setShowModal(false)}>No</button>
-                  <button onClick={(event) => handleDeleteAccount(event, account._id) }>Yes</button>
-                </div>
-              )}
-          </li>
-        ))}
-      </ul>
-      <form onSubmit={handleAddAccount}>
-        <label>
-          Name:
-          <input type="text" value={newAccount.name} onChange={handleNameChange} />
-        </label>
-        <label>
-          IBAN:
-          <input type="text" value={newAccount.iban} onChange={handleIbanChange} />
-        </label>
-        <label>
-          Reference:
-          <select value={newAccount.reference} onChange={handleReferenceChange}>
-            <option value="name">Name</option>
-            <option value="iban">IBAN</option>
-          </select>
-        </label>
-        <button type="submit">Add Account</button>
-      </form>
-    </div>
-  );
+interface IBankAccountData {
+  _id: string;
+  name: string;
+  iban: string;
+  reference: "name" | "iban";
 
 }
-export default BankAccounts;
+
+const BE_URL = import.meta.env.VITE_BE_PORT
+
+const BankAccounts = () => {
+
+  const [bankAccountData, setBankAccountData] = useState<IBankAccountData[]>([]);
+  const [showModal, setShowModal] = useState(false);
+
+  const fetchAccounts = async () => {
+    try {
+      const response = await axios.get(`${BE_URL}/account/getAllMy`,{ withCredentials: true });
+
+      setBankAccountData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+
+useEffect(() => {
+  fetchAccounts();
+}, []);
+
+const addAccount = async (newAccount: IBankAccountData) => {
+  try {
+    const response = await axios.post(`${BE_URL}/accounts/add`, newAccount, { withCredentials: true });
+  } catch (error) {
+    console.log(error);
+  }
+
+  const updateAccount = async (account: IBankAccountData) => {
+    try {
+      const response = await axios.put(`${BE_URL}/accounts/updateMy`, account, { withCredentials: true });
+    } catch (error) {
+      console.log(error);
+    }
+
+    const deleteAccount = async (id: string) => {
+      try {
+        const response = await axios.delete(`${BE_URL}/accounts/delete/${id}`, { withCredentials: true });
+      } catch (error) {
+        console.log(error);
+      }
+
+
+
+
+      export default BankAccounts;
+
+
+// import React, { useEffect, useState } from "react";
+// import useAccountStore from "../context/Accountstore";
+// import { IBankAccountData } from "../context/Accountstore";
+
+// const BankAccounts = () => {
+//   const { bankAccountData, fetchBankAccountData, addBankAccount, updateBankAccount, deleteBankAccount } = useAccountStore();
+//   const [showModal, setShowModal] = useState(false);
+//   const [newAccount, setNewAccount] = useState<IBankAccountData>({ _id: "", name: "", iban: "", reference: "name"});
+
+//   const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     setNewAccount({ ...newAccount, name: event.target.value });
+//   };
+
+//   const handleIbanChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     setNewAccount({ ...newAccount, iban: event.target.value });
+//   };
+
+//   const handleReferenceChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+//     setNewAccount({ ...newAccount, reference: event.target.value as "name" | "iban"});
+//   };
+
+//   const handleAddAccount = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     addBankAccount(newAccount);
+//     setNewAccount({ _id: "", name: "", iban: "", reference: "name" });
+//   };
+
+//   const handleUpdateAccount = (event: React.MouseEvent<HTMLButtonElement>, account: IBankAccountData) => {
+//     event.preventDefault();
+//     console.log(account);
+//     updateBankAccount(account);
+//   };
+
+//   const handleDeleteAccount = (event: React.MouseEvent<HTMLButtonElement>, id: string) => {
+//     event.preventDefault();
+//     deleteBankAccount(id);
+//     setShowModal(false)
+//   };
+
+//   useEffect(() => {
+//     fetchBankAccountData();
+//   }, []);
+
+//   return (
+//     <div>
+//       <h2>Bank Accounts</h2>
+//       <ul>
+//         {bankAccountData && bankAccountData.map((account) => (
+//           <li key={account._id}>
+//             <form>
+//               <label>
+//                 Name:
+//                 <input type="text" value={account.name} onChange={(event) => updateBankAccount({ ...account, name: event.target.value })} />
+//               </label>
+//               <label>
+//                 IBAN:
+//                 <input type="text" value={account.iban} onChange={(event) => updateBankAccount({ ...account, iban: event.target.value })} />
+//               </label>
+//               <label>
+//                 Reference:
+//                 <select value={account.reference} onChange={(event) => updateBankAccount({ ...account, reference: event.target.value as "name" | "iban" })}>
+//                   <option value="name">Name</option>
+//                   <option value="iban">IBAN</option>
+//                 </select>
+//               </label>
+//               <button onClick={(event) => handleUpdateAccount(event, account)}>Update</button>
+//               <button onClick={()=> setShowModal(true)}>Delete</button>
+//             </form>
+//               {showModal && (
+//                 <div>
+//                   <h1>Are you sure you want to delete this account?</h1>
+//                   <button onClick={() => setShowModal(false)}>No</button>
+//                   <button onClick={(event) => handleDeleteAccount(event, account._id) }>Yes</button>
+//                 </div>
+//               )}
+//           </li>
+//         ))}
+//       </ul>
+//       <form onSubmit={handleAddAccount}>
+//         <label>
+//           Name:
+//           <input type="text" value={newAccount.name} onChange={handleNameChange} />
+//         </label>
+//         <label>
+//           IBAN:
+//           <input type="text" value={newAccount.iban} onChange={handleIbanChange} />
+//         </label>
+//         <label>
+//           Reference:
+//           <select value={newAccount.reference} onChange={handleReferenceChange}>
+//             <option value="name">Name</option>
+//             <option value="iban">IBAN</option>
+//           </select>
+//         </label>
+//         <button type="submit">Add Account</button>
+//       </form>
+//     </div>
+//   );
+
+// }
+// export default BankAccounts;
 
 
 
---
+++ src/pages/idea.md
@@ -0,0 +1,17 @@
+//are we userprofile? if yes -> render userdata
+
+
+# 1. Anzeige von Konten -> read ->pages
+- wir brauchen displa backend data von accountkonto
+useState für Modal (isOpen bei klick auf edit)
+id mitschicken an modal damit nur das ausgewählte card bearbeitet wird
+-> usestate seteditedCard (object id vom backend)
+
+
+-> cards mappen (name-iban, symbolbild bank?)
+
+# 2. Modal für CUD
+
+
+update: muss 
+
