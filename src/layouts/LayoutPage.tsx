4b241775330929d4422de7dd0dd02d2a94fe6396
QD2022 <104011263+QD2022@users.noreply.github.com>
2023-04-25 13:42:24 +0200

    LayoutPage fixed

--- src/layouts/LayoutPage.tsx
+++ src/layouts/LayoutPage.tsx
@@ -3,11 +3,11 @@ import Nav from "./Nav";
 
 function LayoutPage() {
   return (
-    <div className="grid grid-cols-12 h-screen m-0 p-0">
+    <div className="grid grid-cols-12 h-full m-0 p-0 bg-neutral-200">
       <div className="col-span-2">
         <Nav />
       </div>
-      <div className="col-span-10">
+      <div className="col-span-10 ">
         <LayoutContent />
       </div>
     </div>
