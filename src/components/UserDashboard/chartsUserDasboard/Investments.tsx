793b5b4568bd462069ebfc78ce5436af9be5dfd7
SebastianWolter1 <seb@techtakel.berlin>
2023-04-27 16:10:39 +0200

    added dummydata for chartjs

--- src/components/UserDashboard/chartsUserDasboard/Investments.tsx
+++ src/components/UserDashboard/chartsUserDasboard/Investments.tsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
 import { Doughnut } from "react-chartjs-2";
 import { ChartConfiguration } from 'chart.js';
+import { invests } from "./Investdata";
 
 
 interface InvestmentProps {
-    setClickedData: Function
+  setClickedData: Function
 }
 
 const Investments = ({setClickedData}: InvestmentProps) => {
-  const stocks = 3000;
-  const krypto = 2000;
-  const realEstate = 4000;
+  const stocks = invests.filter((invest) => invest.type === "Stocks").reduce((acc, curr) => acc + curr.value, 0);
+  const krypto = invests.filter((invest) => invest.type === "Krypto").reduce((acc, curr) => acc + curr.value, 0);
+  const realEstate = invests.filter((invest) => invest.type === "Real Estate").reduce((acc, curr) => acc + curr.value, 0);
 
   const data = {
     labels: ["Stocks", "Krypto", "Real Estate"],
@@ -25,7 +26,6 @@ const Investments = ({setClickedData}: InvestmentProps) => {
   };
 
   const [chartData, setChartData] = useState(data);
- 
 
   const options: ChartConfiguration['options'] = {
     maintainAspectRatio: false,
@@ -33,23 +33,37 @@ const Investments = ({setClickedData}: InvestmentProps) => {
       if (elements.length > 0 && elements[0].index !== undefined) {
         const index = elements[0].index;
         const label = data.labels[index];
+        let innerData: number[] = [];
+        if (label === "Stocks") {
+          innerData = invests.filter((invest) => invest.type === "Stocks").map((invest) => invest.value);
+        } else if (label === "Krypto") {
+          innerData = invests.filter((invest) => invest.type === "Krypto").map((invest) => invest.value);
+        } else if (label === "Real Estate") {
+          innerData = invests.filter((invest) => invest.type === "Real Estate").map((invest) => invest.value);
+        }
         const value = data.datasets[0].data[index];
+        console.log(value, label)
         setChartData({
           labels: [label],
           datasets: [
             {
-              label: "Asset Value",
+              label: "Total Value",
               data: [value],
+              backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+              hoverBackgroundColor: ["#FF6384"],
+            },
+            {
+              label: "Asset Value",
+              data: innerData,
               backgroundColor: ["#008000"],
               hoverBackgroundColor: ["#008000"],
             },
           ],
-        })
-       setClickedData({label, value})
+        });
+        setClickedData({label, value})
       } else {
         setClickedData(undefined)
         setChartData(data);
-   
       }
     },
   };
