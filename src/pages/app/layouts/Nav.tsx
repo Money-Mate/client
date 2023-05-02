abb8e5d2d972cf4ba2da5112aa4e939a0b7ecc47
Pterodactylae <leonie.schwitzkowski@dci-student.de>
2023-05-02 15:57:01 +0200

    routing

--- src/pages/app/layouts/Nav.tsx
+++ src/pages/app/layouts/Nav.tsx
@@ -1,8 +1,21 @@
 import { NavLink } from "react-router-dom";
 import LogoutButton from "../../app/userProfile/LogoutButton";
 
+interface Sublink {
+  name: string;
+  to: string;
+  id: number;
+}
+
+interface Item {
+  name: string;
+  to: string;
+  sublinks?: Sublink[];
+  id: number;
+}
+
 function Nav() {
-  const items = [
+  const items: Item[] = [
     {
       name: "UserDashboard",
       to: "/app/userdashboard",
@@ -17,35 +30,56 @@ function Nav() {
     {
       name: "Investments",
       to: "/app/userdashboard",
+      sublinks: [
+        { name: "Transaktionen", to: "/app/transactionsinvest", id: 1 },
+        { name: "Analyse", to: "/app/analyseinvest", id: 2 },
+      ],
       id: 3,
     },
     {
       name: "Finanzgesundheit",
       to: "/app/",
       id: 4,
-    },
-    {
-      name: "Userprofil",
-      to: "/app/userprofil",
-      id: 5,
-    },
+    }
   ];
 
+  const renderSublinks = (sublinks: Sublink[]) => {
+    return (
+      <ul>
+        {sublinks.map((sublink) => (
+          <li key={sublink.id} className="ml-5 text-mm-text-dark font-semibold ">
+            <NavLink to={sublink.to}>{sublink.name}</NavLink>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
-    
-    <div className="h-full w-full bg-mm-foreground  p-5">
-     <h1 className="text-white mb-5 text-3xl font-bold">MoneyMate</h1>
-     <ul className="text-white p-3 tracking-wide leading-loose">
-                {" "}
-                {items.map((item) => {
-                  return (
-                    <li className="text-blue" key={item.id}>
-                      <NavLink to={item.to}>{item.name}</NavLink>
-                    </li>
-                  );
-                })}
-              </ul>
- 
+    <div className="relative h-full w-full bg-mm-foreground p-5">
+      <h1 className="mb-5 text-3xl font-bold text-white">MoneyMate</h1>
+      <ul className="p-3 leading-loose tracking-wide text-white">
+        {items.map((item) => (
+          <li className="text-blue font-semibold" key={item.id}>
+            {item.sublinks ? (
+              <div>
+                <NavLink to={item.to}>{item.name}</NavLink>
+                {renderSublinks(item.sublinks)}
+              </div>
+            ) : (
+              <NavLink to={item.to}>{item.name}</NavLink>
+            )}
+          </li>
+        ))}
+      </ul>
+      <div className="h-20 w-20 items-center absolute bottom-20 left-16 justify-center rounded-full bg-gray-100">
+        <NavLink to="/app/userprofile">
+        <img
+          src="https://picsum.photos/200"
+          alt="User Profile Picture"
+          className="h-full w-full rounded-full"
+        /></NavLink>
+      </div>
     </div>
   );
 }
