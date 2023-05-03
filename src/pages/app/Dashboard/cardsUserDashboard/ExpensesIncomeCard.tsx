4a70d2aea4403a59a3bd2a626fc0ff3dfc68fb6b
Pterodactylae <leonie.schwitzkowski@dci-student.de>
2023-05-03 10:02:52 +0200

    padding

--- src/pages/app/Dashboard/cardsUserDashboard/ExpensesIncomeCard.tsx
+++ src/pages/app/Dashboard/cardsUserDashboard/ExpensesIncomeCard.tsx
@@ -4,8 +4,8 @@ import ExpenseIncomeChart from "../chartsUserDasboard/ExpensesIncome";
 function IncomeExpensesCard() {
   return (
     <div className="h-full rounded-md bg-mm-foreground">
-      <h2 className="m-2 text-center text-lg font-semibold text-mm-text-white">
-        Saldo der letzten 6 Monate
+      <h2 className="m-2 text-center text-lg font-semibold text-mm-text-white pt-3">
+        Einnahmen/Ausgaben der letzten 6 Monate
       </h2>
       <div className="flex h-60 w-full items-center justify-center p-1">
         <ExpenseIncomeChart />
--- src/pages/app/Dashboard/cardsUserDashboard/SaldoLastSixMonthCard.tsx
+++ src/pages/app/Dashboard/cardsUserDashboard/SaldoLastSixMonthCard.tsx
@@ -3,7 +3,7 @@ import Saldo from "../chartsUserDasboard/Saldo";
 function SaldoLastSixMonth() {
   return (
     <div className="h-full rounded-md bg-mm-foreground">
-      <h2 className="m-2 text-center text-lg font-semibold text-mm-text-white">
+      <h2 className="m-2 text-center text-lg font-semibold text-mm-text-white pt-3">
         Saldo der letzten 6 Monate
       </h2>
       <div className="flex h-60 w-full items-center justify-center p-1">
