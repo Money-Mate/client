23347cb4a97cbc0467221416b968317b8134bb33
Pterodactylae <leonie.schwitzkowski@dci-student.de>
2023-05-03 11:33:27 +0200

    tooltip working

--- src/pages/app/Dashboard/cardsUserDashboard/WishlistCard.tsx
+++ src/pages/app/Dashboard/cardsUserDashboard/WishlistCard.tsx
@@ -1,30 +1,36 @@
 import Progressbar from "../../../../components/Progressbar";
 
 function WishlistCard() {
-  const dummy = { Schwimmbad: 90, Urlaub: 10, Hundekostüm: 5, Entenfutter: 20, Boot: 20, Krokodil: 70 };
+  const dummy = {
+    Schwimmbad: 90,
+    Urlaub: 10,
+    Hundekostüm: 5,
+    Entenfutter: 20,
+    Boot: 20,
+    Krokodil: 70,
+  };
 
   const progressBars = Object.entries(dummy).map(([key, value]) => (
-    <div key={key} className="flex items-center justify-between m-2">
-      <div className="w-1/2 text-mm-text-dark" >{key}</div>
-      <div className="w-1/2" >
+    <div key={key} className="m-2 flex items-center justify-between">
+      <div className="w-1/2 text-mm-text-dark">{key}</div>
+      <div className="group relative flex w-1/2">
         <Progressbar percentage={value} />
+        <span
+          className="absolute left-1/2 m-4 mx-auto -translate-x-1/2 translate-y-full rounded-md bg-mm-foreground px-1 
+    text-sm text-gray-100 opacity-0 transition-opacity group-hover:opacity-100 group-hover:z-10"
+        >
+          {value}%
+        </span>
       </div>
     </div>
   ));
 
   return (
-    <div className=" h-full rounded-md  bg-mm-foreground pt-3">
-      <h2 className="m-2 text-center text-lg font-semibold text-mm-text-white">
+    <div className=" h-full rounded-md  bg-mm-foreground">
+      <h2 className="m-2 pt-3 text-center text-lg font-semibold text-mm-text-white">
         Wunschliste
       </h2>
       <div className="p-1">{progressBars}</div>
-
-
-      <button data-tooltip-target="tooltip-light" data-tooltip-style="light" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Light tooltip</button>
-<div id="tooltip-light" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 tooltip">
-    Tooltip content
-    <div className="tooltip-arrow" data-popper-arrow></div>
-</div>
     </div>
   );
 }
