2dca507655d4cf58ee80a8485bd8e1bffd72983f
SebastianWolter1 <seb@techtakel.berlin>
2023-05-03 09:39:40 +0200

    added padding to cards

--- src/pages/app/Dashboard/cardsUserDashboard/BudgetExpensesCard.tsx
+++ src/pages/app/Dashboard/cardsUserDashboard/BudgetExpensesCard.tsx
@@ -3,7 +3,7 @@ import { BudgetChart } from "../chartsUserDasboard/BudgetChartRelative";
 function BudgetExpenses() {
   return (
     <div className="h-full rounded-md bg-mm-foreground shadow-lg">
-      <h2 className="m-2 text-center text-lg font-semibold text-mm-text-white">
+      <h2 className="m-2 pt-3 text-center text-lg font-semibold text-mm-text-white">
         Budget
       </h2>
       <div className="flex h-48 w-full items-center justify-center p-1">
--- src/pages/app/Dashboard/cardsUserDashboard/WishlistCard.tsx
+++ src/pages/app/Dashboard/cardsUserDashboard/WishlistCard.tsx
@@ -3,7 +3,7 @@ import { WunschlisteChart } from "../chartsUserDasboard/Wunschliste";
 function WishlistCard() {
   return (
     <div className=" h-full rounded-md  bg-mm-foreground ">
-      <h2 className="m-2 text-center text-lg font-semibold text-mm-text-white">
+      <h2 className="m-2 pt-3 text-center text-lg font-semibold text-mm-text-white">
         Wunschliste
       </h2>
       <div className="flex h-48 w-full items-center justify-center p-1">
