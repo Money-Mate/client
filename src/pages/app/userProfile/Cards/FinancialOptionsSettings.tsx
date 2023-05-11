710f09f00fdceac099c77ae9d348a512334a7817
Pterodactylae <leonie.schwitzkowski@dci-student.de>
2023-05-11 15:17:15 +0200

    added little info boxes and svgs

--- src/pages/app/userProfile/Cards/FinancialOptionsSettings.tsx
+++ src/pages/app/userProfile/Cards/FinancialOptionsSettings.tsx
@@ -9,6 +9,16 @@ const FinancialOptionsSettings: React.FC = () => {
   const [wishesPercentage, setWishesPercentage] = useState<number>(30);
   const [savingsPercentage, setSavingsPercentage] = useState<number>(20);
   const [error, setError] = useState<string | null>(null);
+  const [showEmergencyFundInfo, setShowEmergencyFundInfo] = useState(false);
+  const [showSplitIncomeInfo, setShowSplitIncomeInfo] = useState(false);
+
+  const handleEmergencyFundInfoToggle = () => {
+    setShowEmergencyFundInfo(!showEmergencyFundInfo);
+  };
+
+  const handleSplitIncomeInfoToggle = () => {
+    setShowSplitIncomeInfo(!showSplitIncomeInfo);
+  };
 
   console.log(user);
 
@@ -19,6 +29,10 @@ const FinancialOptionsSettings: React.FC = () => {
       needsPercentage + wishesPercentage + savingsPercentage;
     if (totalPercentage !== 100) {
       setError("Bitte teile insgesamt 100% auf.");
+      return;
+    }
+    if (emergencyFund < 0) {
+      setError("Bitte gib einen positiven Betrag ein.");
       return;
     }
 
@@ -52,9 +66,31 @@ const FinancialOptionsSettings: React.FC = () => {
         Einstellungen
       </h2>
       <br />
-      <h3 className=" text-lg font-bold text-mm-text-white">
-        Notgroschen:
-      </h3>
+      <h3 className="text-lg font-bold text-mm-text-white">
+  <span className="inline-flex items-center">
+    Notgroschen
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-4 h-4 ml-1 text-gray-400 cursor-pointer"
+      onClick={handleEmergencyFundInfoToggle}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+      />
+    </svg>
+  </span>
+  {showEmergencyFundInfo && (
+    <div className="text-sm text-gray-500 mt-1">
+      This is the emergency fund information.
+    </div>
+  )}
+</h3>
       <div className="mb-4 p-2 shadow-lg">
         <label
           htmlFor="emergencyFund"
@@ -74,8 +110,30 @@ const FinancialOptionsSettings: React.FC = () => {
 
       <br></br>
       <h3 className="mb-2 text-lg font-bold text-mm-text-white">
-        Angestrebte Aufteilung des Einkommens
-      </h3>
+  <span className="inline-flex items-center">
+    Angestrebte Aufteilung des Einkommens
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-4 h-4 ml-1 text-gray-400 cursor-pointer"
+      onClick={handleSplitIncomeInfoToggle}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+      />
+    </svg>
+  </span>
+  {showSplitIncomeInfo && (
+    <div className="text-sm text-gray-500 mt-1">
+      This is the split income information.
+    </div>
+  )}
+</h3>
 
       <div className="mb-4 ml-4 ">
         <label
