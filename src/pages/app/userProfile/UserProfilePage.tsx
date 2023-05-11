4745ca83413ab83702c371c4781b800e8b727c10
Pterodactylae <leonie.schwitzkowski@dci-student.de>
2023-05-11 12:10:19 +0200

    deleted settings

--- src/pages/app/userProfile/UserProfilePage.tsx
+++ src/pages/app/userProfile/UserProfilePage.tsx
@@ -1,12 +1,8 @@
 import React from "react";
 import { useUserStore } from "../../../context/UserStore";
 import BankAccountsCard from "../userProfile/Cards/BankAccountsCard";
-import FinancialHealthCard from "../userProfile/Cards/FinancialHealthCard";
 import ProfileCard from "../userProfile/Cards/ProfileCard";
-import FinancialHealthTest from "./Cards/FinancialHealthTest";
-// import FinancialSettingsTest2 from "./Cards/HealthTest2";
-// import FinancialSettings from "./Cards/HealthTest2";
-import FinancialOptionsSettings from "./Cards/HealthTest2";
+
 const UserProfile: React.FC = () => {
   const { user } = useUserStore();
 
@@ -15,14 +11,9 @@ const UserProfile: React.FC = () => {
       <div className="m-3 flex min-h-screen w-full flex-col items-center bg-mm-background text-mm-text-dark">
         <ProfileCard user={user} />
         <BankAccountsCard />
-        <FinancialHealthCard />
-        <FinancialHealthTest />
-        {/* <FinancialSettings /> */}
-        <FinancialOptionsSettings />
       </div>
     </div>
   );
 };
-
 
 export default UserProfile;
