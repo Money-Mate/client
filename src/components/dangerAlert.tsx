5cb687b31c787f84c55cd1f8a1c41ff856e34a30
Pterodactylae <leonie.schwitzkowski@dci-student.de>
2023-05-16 11:53:45 +0200

    danger/successalerts

---
+++ src/components/dangerAlert.tsx
@@ -0,0 +1,16 @@
+interface dangerAlertProps {
+    message: string | undefined;
+}
+function dangerAlert(props: dangerAlertProps) {
+  return (
+    <div
+      className="mb-4 rounded-lg bg-red-50 p-4 text-sm text-red-800 dark:bg-gray-800 dark:text-red-400"
+      role="alert"
+    >
+      <span className="font-medium">Sorry! Etwas ist schiefgegangen ...</span> {props.message}
+      
+    </div>
+  );
+}
+
+export default dangerAlert;
---
+++ src/components/successAlert.tsx
@@ -0,0 +1,17 @@
+interface successAlertProps {
+  message: string | undefined;
+}
+
+function successAlert(props: successAlertProps) {
+  return (
+    <div
+      className="mb-4 rounded-lg bg-green-50 p-4 text-sm text-green-800 dark:bg-gray-800 dark:text-green-400"
+      role="alert"
+    >
+      <span className="font-medium">Ok!</span>
+      {props.message}
+    </div>
+  );
+}
+
+export default successAlert;
